c4aefe9ea627cea91ccec75ba8a6c242c8b07a5b
Jaranin Boonpee <jaraninb@fwd.co.th>
2021-05-29 14:01:08 +0700

    Added fastify env config

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,37 @@
 import fastify, { FastifyInstance } from 'fastify'
+import fastifyEnv from 'fastify-env'
 import routes from './routes'
+
+const schema = {
+  type: 'object',
+  required: [ 'PORT' ],
+  properties: {
+    PORT: {
+      type: 'string',
+      default: 3000
+    },
+    ENV_NAME: {
+      type: 'string',
+      default: 'dev'
+    }
+  }
+}
+
+const options = {
+  confKey: 'config', // optional, default: 'config'
+  schema: schema,
+  dotenv: {
+    path: `${process.cwd()}/.env`,
+    debug: true
+  }
+}
+declare module 'fastify' {
+  interface FastifyInstance {
+    config: { // this should be same as the confKey in options
+      // specify your typing here
+    };
+  }
+}
 
 interface IQuerystring {
   username: string
@@ -16,6 +48,7 @@ server.get('/ping', async (request, reply) => {
   return 'pong\n'
 })
 
+server.register(fastifyEnv, options)
 server.register(routes)
 
 server.listen(8080, '0.0.0.0', (err, address) => {
@@ -23,5 +56,6 @@ server.listen(8080, '0.0.0.0', (err, address) => {
     console.error(err)
     process.exit(1)
   }
+  console.log(server.config)
   console.log(`Server listening at ${address}`)
 })
