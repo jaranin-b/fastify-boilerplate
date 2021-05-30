d387a46b5d87ce7b8d932d0ffdb1595a36e9100a
Jaranin Boonpee <jaraninb@fwd.co.th>
2021-05-30 17:56:11 +0700

    Initial typeorm integration

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,23 @@
 import fastify, { FastifyInstance } from 'fastify'
 import fastifyEnv from 'fastify-env'
 import routes from './routes'
+import 'reflect-metadata'
+import { createConnection } from 'typeorm'
+import { User } from './entity/User'
 
 const schema = {
   type: 'object',
-  required: [ 'PORT' ],
+  required: ['PORT'],
   properties: {
     PORT: {
       type: 'string',
-      default: 3000
+      default: 3000,
     },
     ENV_NAME: {
       type: 'string',
-      default: 'dev'
-    }
-  }
+      default: 'dev',
+    },
+  },
 }
 
 const options = {
@@ -22,14 +25,15 @@ const options = {
   schema: schema,
   dotenv: {
     path: `${process.cwd()}/.env`,
-    debug: true
-  }
+    debug: true,
+  },
 }
 declare module 'fastify' {
   interface FastifyInstance {
-    config: { // this should be same as the confKey in options
+    config: {
+      // this should be same as the confKey in options
       // specify your typing here
-    };
+    }
   }
 }
 
@@ -42,20 +46,38 @@ interface IHeaders {
   'h-Custom': string
 }
 
-const server: FastifyInstance = fastify()
+// type orm
+createConnection()
+  .then(async (connection) => {
+    console.log('Inserting a new user into the database...')
+    const user = new User()
+    user.firstName = 'Timber'
+    user.lastName = 'Saw'
+    user.age = 25
+    await connection.manager.save(user)
+    console.log('Saved a new user with id: ' + user.id)
 
-server.get('/ping', async (request, reply) => {
-  return 'pong\n'
-})
+    console.log('Loading users from the database...')
+    const users = await connection.manager.find(User)
+    console.log('Loaded users: ', users)
 
-server.register(fastifyEnv, options)
-server.register(routes)
+    console.log('Here you can setup and run express/koa/any other framework.')
+    const server: FastifyInstance = fastify()
 
-server.listen(8080, '0.0.0.0', (err, address) => {
-  if (err) {
-    console.error(err)
-    process.exit(1)
-  }
-  console.log(server.config)
-  console.log(`Server listening at ${address}`)
-})
+    server.get('/ping', async (request, reply) => {
+      return 'pong\n'
+    })
+
+    server.register(fastifyEnv, options)
+    server.register(routes)
+
+    server.listen(8080, '0.0.0.0', (err, address) => {
+      if (err) {
+        console.error(err)
+        process.exit(1)
+      }
+      console.log(server.config)
+      console.log(`Server listening at ${address}`)
+    })
+  })
+  .catch((error) => console.log(error))
