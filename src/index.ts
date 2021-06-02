d5fb35f57655b6351cd520ca8c6efa84c63a4a70
Jaranin Boonpee <jaraninb@fwd.co.th>
2021-06-02 17:09:44 +0700

    Removed comment code

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,6 @@ import fastifyEnv from 'fastify-env'
 import routes from './routes'
 import 'reflect-metadata'
 import { createConnection } from 'typeorm'
-import { User } from './entity/User'
 
 const schema = {
   type: 'object',
@@ -31,28 +30,14 @@ const options = {
 declare module 'fastify' {
   interface FastifyInstance {
     config: {
-      // this should be same as the confKey in options
-      // specify your typing here
+      PORT: number
     }
   }
 }
 
-// type orm
+// type orm connection
 createConnection()
   .then(async (connection) => {
-    console.log('Inserting a new user into the database...')
-    // const user = new User()
-    // user.firstName = 'Timber'
-    // user.lastName = 'Saw'
-    // user.age = 25
-    // await connection.manager.save(user)
-    // console.log('Saved a new user with id: ' + user.id)
-
-    // console.log('Loading users from the database...')
-    // const users = await connection.manager.find(User)
-    // console.log('Loaded users: ', users)
-
-    console.log('Here you can setup and run express/koa/any other framework.')
     const server: FastifyInstance = fastify()
 
     server.get('/ping', async (request, reply) => {
